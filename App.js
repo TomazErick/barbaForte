f4e0fb3c2b1a9161e3c359af6e6f3e9e1c2c06f3
TomazErick <103327884+TomazErick@users.noreply.github.com>
2022-04-09 13:58:03 -0300

    commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Local
+import MainStack from './src/stacks/MainStack';
+import UserContextProvider from './src/contexts/UserContext';
+
+export default () => {
+  return (
+    <UserContextProvider>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </UserContextProvider>
+  );
+};
